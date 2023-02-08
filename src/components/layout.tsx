bd7e45619e293797584efe5e409daf6c0f7bcb30
Van Diemen Laboratoria UG <vandiemenlaboratoria@proton.me>
2023-02-08 12:10:13 +0100

    Fix props usage

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -2,13 +2,17 @@ import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
 import styles from "@/styles/Home.module.css";
-import { useState } from "react";
+import { ReactNode, useState } from "react";
 import Impressum from "./impressum";
 import { Inter } from "@next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export default function Layout({ children }) {
+type LayoutProps = {
+    children: ReactNode;    
+}
+
+export default function Layout({ children }:LayoutProps) {
 	const [showImpressum, setShowImpressum] = useState(false);
 
 	function toggleImpressum() {
